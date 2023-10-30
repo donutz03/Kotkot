4430c850500a5341037ea302ac3c31f26570d076
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-10-30 09:06:26 +0200

    Create new.kt

---
+++ new.kt
@@ -0,0 +1,11 @@
+var count: Int = 10
+val count: Int = 10
+var count: Int = 10
+val count: Int = 10
+count = count + 1
+count = count + 1
+count = count + 1
+var count = 10
+count = 10
+count++
+count--
